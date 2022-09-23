c5865cf3e8dfc38d8dd4811285b1512f42916d9d
yoshiki301 <yoshiki301@gmail.com>
2022-09-23 23:31:24 +0900

    [add] true value of estimand and test (ongoing)

--- tests/testthat/test-calculate-estimand.R
+++ tests/testthat/test-calculate-estimand.R
@@ -8,23 +8,33 @@ estimators <- data.frame(
   lambda_r_ItMt = c(0.02, 0.03, 0.04),
   lambda_r_ItMc = c(0.04, 0.06, 0.08)
 )
+dataset <- data.frame(
+  induction = c("Ic", "It", "Ic", "It"),
+  maintenance = c("Mc", "Mc", "Mt", "Mt"),
+  theta = c(0.2, 0.3, 0.4, 0.5),
+  lambda_IM_r = c(0.04, 0.04, 0.02, 0.01),
+  lambda_I_nr = c(0.02, 0.04, 0.06, 0.08)
+)
 
 test_that("calc_induction_rmst works", {
-  rmst <- calc_induction_rmst(estimators, t_judge = 6)
+  rmst <- calc_induction_rmst(estimators, dataset, t_judge = 6)
   # TODO: expected values
   # TODO: fix CI
+  expect_equal(T, T)
 })
 
 test_that("calc_maintenance_hr works", {
-  hr <- calc_maintenance_hr(estimators)
+  hr <- calc_maintenance_hr(estimators, dataset)
   # TODO: fix CI
   expected_hr <- data.frame(
     hr_Ic = c(0.5, 0.5, 0.5),
     hr_Ic_lower_ci = c(0.0, 0.0, 0.0),
     hr_Ic_upper_ci = c(1000, 1000, 1000),
+    hr_Ic_true = rep(0.5, 3L),
     hr_It = c(0.5, 0.5, 0.5),
     hr_It_lower_ci = c(0.0, 0.0, 0.0),
-    hr_It_upper_ci = c(1000, 1000, 1000)
+    hr_It_upper_ci = c(1000, 1000, 1000),
+    hr_It_true = rep(0.25, 3L)
   )
   expect_equal(hr, expected_hr)
 })
