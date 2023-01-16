696ad0f990f576d491002998e857b6e625a8a49f
yoshiki301 <yoshiki301@gmail.com>
2023-01-16 21:04:20 +0900

    [add] small test cases

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -4,7 +4,7 @@ dataset <- generate_scenario(
   IcMc_size = 10L, ItMc_size = 10L, IcMt_size = 10L, ItMt_size = 10L
 )
 
-test_that("calc_responsibility works", {
+test_that("estimateEM_bootstrap_ci works", {
   boot_ci <- estimateEM_bootstrap_ci(dataset, boot_num = 5L, alpha = 0.05)
   expect_equal(nrow(boot_ci), 2)
   expect_equal(ncol(boot_ci), 5)
--- tests/testthat/test-calculate-estimand.R
+++ tests/testthat/test-calculate-estimand.R
@@ -1,1 +1,26 @@
 # TODO: add test case
+test_that("rmst works", {
+  expect_equal( # scalar
+    rmst(0.5, 1.0, 6),
+    3.498761,
+    tolerance = 1e-5
+  )
+  expect_equal( # vector
+    rmst(rep(0.5, 5L), rep(1.0, 5L), rep(6, 5L)),
+    rep(3.498761, 5L),
+    tolerance = 1e-5
+  )
+})
+
+test_that("hr works", {
+  expect_equal( # scalar
+    hr(0.6, 0.8),
+    0.75,
+    tolerance = 1e-5
+  )
+  expect_equal( # vector
+    hr(rep(0.6, 5L), rep(0.8, 5L)),
+    rep(0.75, 5L),
+    tolerance = 1e-5
+  )
+})
